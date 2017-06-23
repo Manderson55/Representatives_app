d57933d820a706ffbd32500af8047b168228f1e3
Devon Jones <devon.jones@homeadvisor.com>
2017-06-22 20:33:16 -0600

    Created elected offical object, added FB stuff

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,37 @@
-$(document).ready(function() {
+//Elected Offical Object
+var ElectedOfficial ={
+  "firstName": "",
+  "lastName": "",
+  "streetNumber": "",
+  "streetName": "",
+  "state": "",
+  "zip": "",
+  "role": "",
+  "phoneNum": "",
+  "website": "",
+  "party": "",
+  "imgSrc": "",
+  "totalMoney": "",
+  "spent": "",
+  "onHand": "",
+  "top3IndustryDonors": [],
+  "top3Contributors": []
+};
 
+// Creating new instance of an elected official
+ var rep = Object.create(ElectedOfficial);
 
-});
+// Firebase
+var config = {
+  apiKey: "AIzaSyCi-dzO9wATSlwXs5EJCzBMvKjGflTy850",
+  authDomain: "voterapp-839b2.firebaseapp.com",
+  databaseURL: "https://voterapp-839b2.firebaseio.com",
+  projectId: "voterapp-839b2",
+  storageBucket: "voterapp-839b2.appspot.com",
+  messagingSenderId: "96769787339"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+$(document).ready(function() {})
